a07a2c48bd1e9eee683c69fc5d3b4a290153a823
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-20 20:03:45 +0530

    ADDED : Code to store employee payroll into a file..

--- src/main/java/org/example/EmployeePayrollService.java
+++ src/main/java/org/example/EmployeePayrollService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+    public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
     private List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollService() {}
 
     public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
         this.employeePayrollList = employeePayrollList;
@@ -17,7 +20,7 @@ public class EmployeePayrollService {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
         Scanner consoleInputReader = new Scanner(System.in);
         employeePayrollService.readEmployeePayrollData(consoleInputReader);
-        employeePayrollService.writeEmployeePayrollData();
+        employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.CONSOLE_IO);
     }
 
     private void readEmployeePayrollData(Scanner consoleInputReader) {
@@ -30,7 +33,11 @@ public class EmployeePayrollService {
         employeePayrollList.add(new EmployeePayrollData(id, name, salary));
     }
 
-    private void writeEmployeePayrollData() {
+    public void writeEmployeePayrollData(IOService ioService) {
+        if(ioService.equals(IOService.CONSOLE_IO))
         System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+        else if (ioService.equals(IOService.FILE_IO)) {
+            new EmployeePayrollFileIOService().writeData(employeePayrollList);
+        }
     }
 }
---
+++ src/test/java/testpackage/EmployeePayrollServiceTest.java
@@ -0,0 +1,22 @@
+package testpackage;
+
+import org.example.EmployeePayrollData;
+import org.example.EmployeePayrollService;
+import org.junit.Test;
+import java.util.Arrays;
+
+import static org.example.EmployeePayrollService.IOService;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries() {
+        EmployeePayrollData [] arrayOfEmps = {
+                new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Mark ZuckerBerg", 300000.0)
+        };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+        employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+    }
+}
