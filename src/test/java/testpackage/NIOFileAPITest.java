0b03a9d31f64cd7c4c39e61ac45921e9f8d9d0cd
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-20 18:59:14 +0530

    ADDED : Code for creating watch service.

--- src/test/java/testpackage/NIOFileAPITest.java
+++ src/test/java/testpackage/NIOFileAPITest.java
@@ -43,4 +43,11 @@ public class NIOFileAPITest {
                 path.toString().startsWith("temp"))
                 .forEach(System.out::println);
     }
+
+    @Test
+    public void givenADirectory_WhenWatchedLists_AllTheActivities() throws IOException {
+        Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchServiceExample(dir).processEvents();
+    }
 }
